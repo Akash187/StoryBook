1f769c35b0ed1758444baa9075ef79536ad3cdcc
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-02-17 23:36:19 +0530

    added route for public stories, login and about. About page completed.

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 // Welcome Page
 router.get('/', (req, res) => res.render('home', {title: 'Home Page'}));
 
+//About Page
+router.get('/about', (req, res) => res.render('about', {title: 'About Page'}));
+
 module.exports = router;
