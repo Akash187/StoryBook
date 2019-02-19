c9cbd9879885839d3a42b1b1ef2af496c2bcb20c
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-02-19 11:27:08 +0530

    added feature to submit form, validation and image upload.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "uploads",
+  allowedFormats: ["jpg", "png", "jpeg", "gif"],
+  transformation: [{ width: 480, height: 480, crop: "fill" }]
+});
+
+const parser = multer({ storage: storage }).single("myImage");
+
+router.post('/register', (req, res) => {
+    parser(req, res, (err) => {
+    if(err){
+      console.log('first err', err);
+      res.send({
+        msg: err
+      });
+    } else {
+      if(req.file === undefined){
+        res.status(400).send({
+          msg: 'Error: Unable to upload File!'
+        });
+      } else {
+        console.log('File Uploaded!');
+        console.log(req.file); // to see what is returned to you
+        console.log(req.body);
+        const image = {};
+        image.url = req.file.url;
+        image.id = req.file.public_id;
+        res.redirect('/users/login');
+        // cloudinary.uploader.destroy(image.id, function(result) { console.log(result) });
+        // Image.create(image) // save image information in database
+        //   .then(newImage => res.json(newImage))
+        //   .catch(err => console.log(err));
+      }
+    }
+  });
+});
 
 // Welcome Page
 router.get('/', (req, res) => res.render('home', {title: 'Home Page'}));
--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dotenv =  require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.engine('hbs', exphbs({defaultLayout: 'main.hbs'}));
