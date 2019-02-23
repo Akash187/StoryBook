abd624e2bdde8f3b983ba978c365d2ce97f2cbeb
Akash Kumar Seth <kumarakashseth007@gmail.com>
2019-02-24 02:14:49 +0530

    added promise to mongoose.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/StoryBook', {useNewUrlParser: true});
 
 module.exports = {mongoose};
